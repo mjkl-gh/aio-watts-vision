6ea5cf53965e46e5196ae628b007e3d52e5a14e9
Maarten Kleijwegt <mkleijwegt@lely.com>
2022-12-21 22:51:19 +0100

    Split Smarthome and SmarthomeData

--- aio_watts_vision/models/objects.py
+++ aio_watts_vision/models/objects.py
@@ -1,4 +1,3 @@
-from ..auth import auth
 from .base import BaseAPIObject
 from typing import List
 
@@ -7,7 +6,59 @@ from typing import List
 class Smarthome(BaseAPIObject):
     """Class that represents a smarthome in the Watts Vision API"""
 
+    @property    
+    def label(self) -> str:
+        """Return the label of the smarthome."""
+        return self.attributes.get("label", None)
+
+
     @property
+    def latitude(self) -> float:
+        """Return the latitude of the smarthome."""
+        return self.attributes.get("latitude", None)
+
+
+    @property
+    def longitude(self) -> float:
+        """Return the longitude of the smarthome."""
+        return self.attributes.get("longitude", None)
+
+
+    @property
+    def address_position(self) -> str:
+        """Return the address_position of the smarthome."""
+        return self.attributes.get("address_position", None)
+
+    @property
+    def general_mode(self) -> bool:
+        """Return the general mode of the smarthome."""
+        return self.attributes.get("general_mode", None)
+
+    @property
+    def holiday_mode(self) -> str:
+        """Return the holiday_mode  of the smarthome."""
+        return self.attributes.get("holiday_mode", None)
+
+    @property
+    def mac_address(self) -> str:
+        """Return the mac_address  of the smarthome."""
+        return self.attributes.get("mac_address", None)
+
+    @property
+    def param_c_f(self) -> int:
+        """Return the param_c_f  of the smarthome."""
+        return self.attributes.get("param_c_f", None)
+
+
+    @property
+    def smarthome_id(self) -> str:
+        """Return the id of the smarthome."""
+        return self.attributes.get("smarthome_id", None)
+
+class SmarthomeData(Smarthome):
+    """Class that represents a smarthome in the Watts Vision API"""
+    
+    @property      
     def label(self) -> str:
         """Return the label of the smarthome."""
         return self.attributes.get("label", None)
@@ -32,7 +83,7 @@ class Smarthome(BaseAPIObject):
 
 
     @property
-    def devices(self) -> List[Device]:
+    def devices(self) -> List["Device"]:
         """Return the devices of the smarthome."""
         return list(map(Device, self.attributes.get("devices", [])))
 
@@ -305,3 +356,60 @@ class Zone(BaseAPIObject):
         """Return the zone_label of the smarthome."""
         return self.attributes.get("zone_label", None)
 
+class Mode(BaseAPIObject):
+    @property
+    def bundle_id(self) ->  int:
+        """Return the bundle_id of the smarthome."""
+        return self.attributes.get("bundle_id", None)
+
+
+    @property
+    def nvgv_mode_id(self) ->  int:
+        """Return the nvgv_mode_id of the smarthome."""
+        return self.attributes.get("nvgv_mode_id", None)
+
+
+    @property
+    def smarthome_id(self) ->  str:
+        """Return the smarthome_id of the smarthome."""
+        return self.attributes.get("smarthome_id", None)
+
+
+    @property
+    def smarthome_mode_type_id(self) ->  int:
+        """Return the smarthome_mode_type_id of the smarthome."""
+        return self.attributes.get("smarthome_mode_type_id", None)
+
+class User(BaseAPIObject):
+    
+    @property
+    def email(self) ->  str:
+        """Return the email of the User."""
+        return self.attributes.get("email", None)
+
+    @property
+    def user_id(self) ->  str:
+        """Return the user_id of the User."""
+        return self.attributes.get("user_id", None)
+
+    @property   
+    def lang_code(self) ->  str:
+        """Return the lang_code of the User."""
+        return self.attributes.get("lang_code", None)
+
+    @property   
+    def cgu_id(self) ->  int:
+        """Return the cgu_id of the User."""
+        return self.attributes.get("cgu_id", None)\
+
+    @property   
+    def optin_stats(self) ->  bool:
+        """Return the optin_stats of the User."""
+        return self.attributes.get("optin_stats", None)
+
+    @property   
+    def smarthomes(self) ->  List["Smarthome"]:
+        """Return the smarthomes of the User."""
+        return self.attributes.get("smarthomes", None)    
+
+        
