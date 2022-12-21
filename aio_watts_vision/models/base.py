2b9e4893df7513d3ea07d19a8a404c4a422941d8
Maarten Kleijwegt <mkleijwegt@lely.com>
2022-12-21 22:47:29 +0100

    Remove APIObject Implementation

--- aio_watts_vision/models/base.py
+++ aio_watts_vision/models/base.py
@@ -6,14 +6,9 @@ class BaseObject():
 
     logger = logging.getLogger(__name__)
 
-    def __init__(self, attributes) -> None:
+    def __init__(self, attributes: dict) -> None:
         """Initialize."""
         self.attributes = attributes
 
 class BaseAPIObject(BaseObject):
     """Base class for the Watts vision REST API."""
-
-    def __init__(self, client: "AIOWattsVisionAPIClient", attributes: dict) -> None:
-        """Initialise."""
-        super().__init__(attributes)
-        self.client = client        
