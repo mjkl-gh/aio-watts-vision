8f58b2f86282d569e0f1db6e8ecc5e5bfdf1f182
Maarten Kleijwegt <mkleijwegt@lely.com>
2022-12-24 11:39:56 +0100

    Add __main__.py

--- aio_watts_vision/__main__.py
+++ aio_watts_vision/__main__.py
@@ -1,17 +1,46 @@
 import asyncio
 import aiohttp
+import logging
+from pprint import pprint
 
-from aio_watts_vision.auth import Auth
+from token_manager import TokenManager
+from auth import Auth
+from models.enums import Mode
+from models.smarthome import Smarthome
+from watts_vision_api import WattsVisionAPI
+
+_LOGGER = logging.getLogger(__name__)
+
+_LOGGER.setLevel("DEBUG")
+
+USERNAME = 
+PASSWORD = 
+
+WATTS_VISION_URL = "https://smarthome.wattselectronics.com" 
+WATTS_VISION_AUTH_URL = WATTS_VISION_URL + "/auth/realms/watts/protocol/openid-connect/token"
+WATTS_VISION_API_URL = WATTS_VISION_URL + "/api/v0.1/human"
+
+_LOGGER = logging.getLogger(__name__)
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
-        auth = Auth(session, "http://example.com/api", "secret_access_token")
+        token_manager = await TokenManager.create(session,  WATTS_VISION_AUTH_URL, USERNAME, PASSWORD)
+        auth = Auth(session, WATTS_VISION_API_URL, token_manager)
+        api= WattsVisionAPI(auth)
 
-        # This will fetch data from http://example.com/api/lights
-        resp = await auth.request("get", "lights")
-        print("HTTP response status code", resp.status)
-        print("HTTP response JSON content", await resp.json())
+        user = await api.async_get_user(USERNAME)
+        pprint(vars(user))
 
+        while True:
+            smarthome_data = await api.async_get_smarthome_data(user.smarthomes[0].smarthome_id)
+            print("Smarthome object:")
+            pprint(vars(smarthome_data), depth=3)
+
+            device = smarthome_data.devices[4]
+            pprint(vars(device))
+            resp = await device.async_set_temp(Mode.BOOST, 20.0) 
+            print(await resp.json())
+            await asyncio.sleep(10)
 
 asyncio.run(main())
